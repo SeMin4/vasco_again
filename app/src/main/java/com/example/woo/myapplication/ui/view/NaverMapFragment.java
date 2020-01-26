2c25dcf639e1ea3df57f2a90da6eb17d17f97d3d
SeMin4 <ohsemin4@gmail.com>
2020-01-27 08:25:08 +0900

    Add recylcerView

--- app/src/main/java/com/example/woo/myapplication/ui/view/NaverMapFragment.java
+++ app/src/main/java/com/example/woo/myapplication/ui/view/NaverMapFragment.java
@@ -7,22 +7,28 @@ import android.support.annotation.Nullable;
 import android.support.annotation.UiThread;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.woo.myapplication.R;
+import com.example.woo.myapplication.utils.GridAdapter;
 import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 public class NaverMapFragment extends Fragment implements OnMapReadyCallback {
-//    private MapView mapView;
     MapFragment mapFragment;
-    TextView tmp;
-
+    ArrayList tmp;
     public static NaverMapFragment newInstance() {
         NaverMapFragment fragment = new NaverMapFragment();
         Bundle args = new Bundle();
@@ -33,6 +39,7 @@ public class NaverMapFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        tmp = new ArrayList<>(Arrays.asList("Person1", "Person2", "Person3", "Person4", "Person5", "Person6", "Person7", "Person8", "Person9", "Person10", "Person11", "Person12", "Person13", "Person14"));
         FragmentManager fm = getChildFragmentManager();
 //        MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map);
         if (mapFragment == null) {
@@ -51,8 +58,18 @@ public class NaverMapFragment extends Fragment implements OnMapReadyCallback {
         // ...
 //        super.onCreateView(inflater,container,savedInstanceState);
         View rootView =  inflater.inflate(R.layout.fragment_map, container, false);
-        TextView tmp = (TextView)rootView.findViewById(R.id.tmp);
-//        tmp.bringToFront();
+        RecyclerView mapGridRecycler = (RecyclerView)rootView.findViewById(R.id.map_grid_recycler);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity().getApplicationContext(), 8 , LinearLayoutManager.HORIZONTAL, false);
+//        gridLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        mapGridRecycler.setLayoutManager(gridLayoutManager);
+        GridAdapter gridAdapter = new GridAdapter(getActivity().getApplicationContext(), tmp);
+        mapGridRecycler.setAdapter(gridAdapter);
+        mapGridRecycler.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return false;
+            }
+        });
         return rootView;
     }
 
