63f8da9319931d8a905b87b12b2ac305782d1736
potatoyum <hellokyk20@gmail.com>
2020-02-07 22:30:08 +0900

    import 컬러피커
    컬러피커 임포팅

--- app/src/main/java/com/example/woo/myapplication/ui/activity/InsertDepActivity.java
+++ app/src/main/java/com/example/woo/myapplication/ui/activity/InsertDepActivity.java
@@ -1,10 +1,9 @@
 package com.example.woo.myapplication.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.example.woo.myapplication.R;
 
